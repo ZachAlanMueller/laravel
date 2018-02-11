e03b5ba96688a0ea04a331709a1f165bb703e759
Zach Mueller <zach.a.mueller@gmail.com>
2018-02-11 11:03:35 -0600

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,280 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Zach Mueller</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="About Zach Mueller" />
+  <meta name="keywords" content="" />
+  <meta name="author" content="Zach Mueller" />
+
+    <!-- 
+  //////////////////////////////////////////////////////
+
+  FREE HTML5 TEMPLATE 
+  DESIGNED & DEVELOPED by FreeHTML5.co
+    
+  Website:    http://freehtml5.co/
+  Email:      info@freehtml5.co
+  Twitter:    http://twitter.com/fh5co
+  Facebook:     https://www.facebook.com/fh5co
+
+  //////////////////////////////////////////////////////
+  -->
+
+    <!-- Facebook and Twitter integration -->
+  <meta property="og:title" content=""/>
+  <meta property="og:image" content=""/>
+  <meta property="og:url" content=""/>
+  <meta property="og:site_name" content=""/>
+  <meta property="og:description" content=""/>
+  <meta name="twitter:title" content="" />
+  <meta name="twitter:image" content="" />
+  <meta name="twitter:url" content="" />
+  <meta name="twitter:card" content="" />
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+  
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css/icomoon.css">
+  <!-- Bootstrap  -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <!-- Flexslider  -->
+  <link rel="stylesheet" href="css/flexslider.css">
+  <!-- Theme style  -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Modernizr JS -->
+  <script src="js/modernizr-2.6.2.min.js"></script>
+  <!-- FOR IE9 below -->
+  <!--[if lt IE 9]>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+  </head>
+  <body>
+  <div id="fh5co-page">
+    <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+    <aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+
+      <h1 id="fh5co-logo"><a href="/">Zach</a></h1>
+      <nav id="fh5co-main-menu" role="navigation">
+        <ul>
+          <li class="fh5co-active"><a href="/">Home</a></li>
+          <li><a href="http://blog.zachalanmueller.com">Blog</a></li>
+          <li><a href="/portfolio">Portfolio</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      <!-- <div class="fh5co-footer">
+        <p><small></small></p>
+        <ul>
+          <li><a href="#"><i class="icon-facebook2"></i></a></li>
+          <li><a href="#"><i class="icon-twitter2"></i></a></li>
+          <li><a href="#"><i class="icon-instagram"></i></a></li>
+          <li><a href="#"><i class="icon-linkedin2"></i></a></li>
+        </ul>
+      </div> -->
+
+    </aside>
+
+    <div id="fh5co-main">
+      <aside id="fh5co-hero" class="js-fullheight">
+        <div class="flexslider js-fullheight">
+          <ul class="slides">
+            <li style="background-image: url(/images/bg-1.jpg);">
+              <div class="overlay"></div>
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+                    <div class="slider-text-inner">
+                      <h1 ><strong>Web Developer.</strong> <br>It's about making your ideas become reality.</h1>
+                      <h2>Check out some of my <a href="http://zachalanmueller.com/portfolio">ideas</a>.</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li style="background-image: url(/images/bg-2.jpg);">
+              <div class="overlay"></div>
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+                    <div class="slider-text-inner">
+                      <h1>Like what you see?</h1>
+                      <h2>A lot of what I do is open source. Checkout my github!</h2>
+                      <p><a class="btn btn-primary btn-demo popup-vimeo" href="https://github.com/ZachAlanMueller"> <i class="fab fa-github fa-2x"></i> Github</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li style="background-image: url(/images/bg-4.jpg);">
+              <div class="overlay"></div>
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+                    <div class="slider-text-inner">
+                      <h1>Want to know more?</h1>
+                      <h2>I have a blog where I go talk about my projects, video games, books, and more!
+                      <p><a class="btn btn-primary btn-demo popup-vimeo" href="http://blog.zachalanmueller.com"> <i class="fas fa-book fa-2x"></i> My Blog</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            </ul>
+          </div>
+      </aside>
+      
+      <div class="fh5co-narrow-content">
+        <h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Services</h2>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+              <div class="fh5co-icon">
+                <i class="icon-settings"></i>
+              </div>
+              <div class="fh5co-text">
+                <h3>Strategy</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+              <div class="fh5co-icon">
+                <i class="icon-search4"></i>
+              </div>
+              <div class="fh5co-text">
+                <h3>Explore</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+              <div class="fh5co-icon">
+                <i class="icon-paperplane"></i>
+              </div>
+              <div class="fh5co-text">
+                <h3>Direction</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+              <div class="fh5co-icon">
+                <i class="icon-params"></i>
+              </div>
+              <div class="fh5co-text">
+                <h3>Expertise</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fh5co-narrow-content">
+        <h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Recent Blog</h2>
+        <div class="row row-bottom-padded-md">
+          <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+            <div class="blog-entry">
+              <a href="#" class="blog-img"><img src="images/img-1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+              <div class="desc">
+                <h3><a href="#">Inspirational Website</a></h3>
+                <span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+                <p>Design must be functional and functionality must be translated into visual aesthetics</p>
+                <a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+            <div class="blog-entry">
+              <a href="#" class="blog-img"><img src="images/img-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+              <div class="desc">
+                <h3><a href="#">Inspirational Website</a></h3>
+                <span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+                <p>Design must be functional and functionality must be translated into visual aesthetics</p>
+                <a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+            <div class="blog-entry">
+              <a href="#" class="blog-img"><img src="images/img-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+              <div class="desc">
+                <h3><a href="#">Inspirational Website</a></h3>
+                <span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+                <p>Design must be functional and functionality must be translated into visual aesthetics</p>
+                <a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+            <div class="blog-entry">
+              <a href="#" class="blog-img"><img src="images/img-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+              <div class="desc">
+                <h3><a href="#">Inspirational Website</a></h3>
+                <span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+                <p>Design must be functional and functionality must be translated into visual aesthetics</p>
+                <a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="get-in-touch">
+        <div class="fh5co-narrow-content">
+          <div class="row">
+            <div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+              <h1 class="fh5co-heading-colored">Get in touch</h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+              <p class="fh5co-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+              <p><a href="#" class="btn btn-primary">Learn More</a></p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- jQuery -->
+  <script src="js/jquery.min.js"></script>
+  <!-- jQuery Easing -->
+  <script src="js/jquery.easing.1.3.js"></script>
+  <!-- Bootstrap -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- Waypoints -->
+  <script src="js/jquery.waypoints.min.js"></script>
+  <!-- Flexslider -->
+  <script src="js/jquery.flexslider-min.js"></script>
+  
+  
+  <!-- MAIN JS -->
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
+
